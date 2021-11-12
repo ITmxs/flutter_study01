e9552496e91391b204d22aec7884f2800be9c204
luckly <17752170152@163.com>
2021-11-12 09:22:07 +0800

    按钮

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,81 @@
-// ignore_for_file: prefer_const_constructors, unnecessary_new
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
 
+/// This Widget is the main application widget.  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      theme: ThemeData(primaryColor: Colors.green.shade800),
-      home: MyHomePage(
-        title: "坚果",
-      ),
+    return const MaterialApp(
+      home: MyStatefulWidget(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({required this.title});
-  // This widget is the home page of your application.
-  final String title;
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
 
   @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.title),
+        title: Text('Flutter Scaffold Example'),
       ),
       body: Center(
+        child: Text('We have pressed the button $_count times.'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+          height: 50.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _count++;
+        }),
+        tooltip: 'Increment Counter',
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      drawer: Drawer(
+        // elevation: 40.0,
         child: Column(
-          children: [
-            Text('Hello World'), //TextButton Example
-            new TextButton(
-              child: Text("Click here"),
-              onPressed: () {
-                // Do something here
-              },
+          children: <Widget>[
+            const UserAccountsDrawerHeader(
+              accountName: Text("坚果前端"),
+              accountEmail: Text("微信：xjg13690"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.yellow,
+                child: Text("abc"),
+              ),
             ),
-
-//OutlinedButton Example
-            new OutlinedButton(
-              child: Text("Click here"),
-              onPressed: () {
-                // Do something here
-              },
+            ListTile(
+              title: new Text("QQ：852851198@qq.com"),
+              leading: new Icon(Icons.mail),
             ),
+            Divider( height: 0.1,),
+            ListTile(
+              title: new Text("11"),
+              leading: new Icon(Icons.inbox),
+            ),
+            ListTile(
+              title: new Text("组织"),
+              leading: new Icon(Icons.people),
+            ),
+            ListTile(
+              title: new Text("加油"),
+              leading: new Icon(Icons.local_offer),
+            )
           ],
         ),
       ),
     );
   }
-}
+}  
